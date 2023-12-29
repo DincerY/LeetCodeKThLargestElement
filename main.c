60738aa8672bf51999eeaad67f2d3c7a165a40a5
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-12-29 10:10:52 +0300

    finished

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int HeapSize = 1;
 
@@ -6,6 +7,11 @@ void swap(int * a,int * b){
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+void printArray(int * arr,int numsSize){
+    for (int i = 0; i < numsSize+1; i++) {
+        printf("[%d]\n",arr[i]);
+    }
 }
 
 //eklerken
@@ -21,13 +27,18 @@ void heapifyUp(int arr[],int lastIndex){
 //silerken
 void heapifyDown(int * arr){
     int currNode = 1;
-    int left = currNode*2;
-    int right = currNode*2+1;
-    while(arr[currNode] < arr[left] || arr[currNode] < arr[right]){
 
+    while(currNode<HeapSize && arr[currNode] < arr[currNode*2] || arr[currNode] < arr[currNode*2+1]){
+        int left = currNode*2;
+        int right = currNode*2+1;
+        if(arr[left] > arr[right]){
+            swap(&arr[currNode],&arr[left]);
+            currNode = left;
+        } else{
+            swap(&arr[currNode],&arr[right]);
+            currNode = right;
+        }
     }
-
-
 }
 
 void insert(int arr[],int value){
@@ -38,41 +49,33 @@ void insert(int arr[],int value){
 int removeMaxElement(int * arr){
     int result = arr[1];
     arr[1] = arr[HeapSize];
+    arr[HeapSize] = 0;
     HeapSize--;
     heapifyDown(arr);
-
-    return 0;
-}
-
-void printArray(int * arr,int numsSize){
-    for (int i = 0; i < numsSize+1; i++) {
-        printf("[%d]\n",arr[i]);
-    }
-}
-
-int findKthLargest(int* nums, int numsSize, int k) {
-    int arr[numsSize+1] = {};
-    int result = 0;
-    for (int i = 0; i < numsSize; i++) {
-        insert(arr,nums[i]);
-    }
-    /*for (int i = 0; i < k; i++) {
-        result = removeMaxElement(arr);
-    }*/
-    printArray(arr,6);
     return result;
 }
 
 
 
-
+int findKthLargest(int* nums, int numsSize, int k) {
+    int arr[numsSize+1];
+    memset(arr,0,sizeof(arr));
+    int result = 0;
+    for (int i = 0; i < numsSize; i++) {
+        insert(arr,nums[i]);
+    }
+    HeapSize--;
+    for (int i = 0; i < k; i++) {
+        result = removeMaxElement(arr);
+    }
+    return result;
+}
 
 
 
 int main() {
-    int arr[] = {3,2,1,5,6,4};
-    findKthLargest(arr,6,3);
-    removeMaxElement(arr);
+    int arr[] = {15,20,2,3,5,7,8,85,35,78,65,11,79,85,85,85,13,45};
+    int deneme = findKthLargest(arr,18,7);
 
-    return 0;
+    return deneme;
 }
